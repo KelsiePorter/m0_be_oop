2e5c7da4e409d1dc1c8d24f331246c7314aa4a32
Kelsie Porter <krporter22@gmail.com>
2022-09-10 13:28:48 -0600

    Add Hobbit class

--- oop.rb
+++ oop.rb
@@ -116,3 +116,71 @@ p dragon4
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+  def initialize(name, disposition, age = 0, is_adult = false, is_old = false, has_ring = false)
+    @name = name
+    @disposition = disposition
+    @age = age
+    @is_adult = false
+    @is_old = false
+    @has_ring = name == "Frodo" ? true : false
+  end
+
+  def celebrate_birthday
+    if @age == 32
+      @is_adult = true
+      @age += 1
+    elsif @age == 100
+      @is_adult = true
+      @is_old = true
+      @age += 1
+    else
+      @age += 1
+    end
+  end
+
+  # def ring
+  #   if @name == "Frodo"
+  #     @has_ring = true
+  #   else
+  #     @has_ring = false
+  #   end
+  # end
+end
+
+hobbit4 = Hobbit.new("Frodo", "cool")
+34.times do
+  hobbit4.celebrate_birthday
+end
+hobbit5 = Hobbit.new("Sam", "cool")
+101.times do
+  hobbit5.celebrate_birthday
+end
+hobbit6 = Hobbit.new("Frodo", "excited")
+107.times do
+  hobbit6.celebrate_birthday
+end
+p hobbit4
+p hobbit5
+p hobbit6
+
+hobbit1 = Hobbit.new("Sam", "nice")
+8.times do
+  hobbit1.celebrate_birthday
+end
+p hobbit1
+#
+# hobbit2 = Hobbit.new("Bodi", "mean")
+# 12.times do
+#   hobbit2.celebrate_birthday
+# end
+# hobbit2.ring
+# p hobbit2
+#
+# hobbit3 = Hobbit.new("Frodo", "rude")
+# 101.times do
+#   hobbit3.celebrate_birthday
+# end
+# hobbit3.ring
+# p hobbit3
