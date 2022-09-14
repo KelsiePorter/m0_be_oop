7e978e2b59ad0a39bad20b1532f984e08d986ff2
Kelsie Porter <krporter22@gmail.com>
2022-09-14 17:16:39 -0600

    add access attributes practice

---
+++ car.rb
@@ -0,0 +1,35 @@
+class Car
+  # attr_reader :make,
+  #             :model,
+  #             :color
+  # attr_writer :color,
+  #             :make,
+  #             :model
+
+  attr_accessor :make,
+                :model,
+                :color
+
+  def initialize(make, model, color)
+    @make = make
+    @model = model
+    @color = color
+  end
+
+  # def make
+  #   @make
+  # end
+
+  # def update_color(new_color)
+  #   @color = new_color
+  # end
+end
+
+car1 = Car.new("Ford", "F150", "hot ass pink")
+
+p car1.make
+p car1.model
+p car1.color
+# p car1.update_color("brown")
+p car1.color = "brown"
+p car1.make = "Honda"
